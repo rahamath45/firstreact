b74b05d7a2a56c6cacc7f8813033412314034bfe
rahamath45 <rahmathsulaiman45@gmail.com>
2025-08-10 01:09:12 +0530

    topcontent slice rechange

--- src/components/Topcontent.jsx
+++ src/components/Topcontent.jsx
@@ -1,7 +1,7 @@
  export default function Topcontent({ convertEpochToTime, courseDetails }) {
   return (
     <div className="flex-row">
-      <div>{new Date().toLocaleString().slice(0, 8)}</div>
+      <div>{new Date().toLocaleString().slice(0, 9)}</div>
       <div>{`${convertEpochToTime(
         courseDetails.startTime
       )} to ${convertEpochToTime(courseDetails.endTime)}`}</div>
